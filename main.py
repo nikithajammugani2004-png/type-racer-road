f0362130c4a9acee96d78aff38737a15ad251921
Nikitha Jammugani <nikithajammugani2004@gmail.com>
2026-08-18 11:58:04 +0530

    Fix index.html file path in main.py

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def read_root():
-    return FileResponse("static/index.html")
+    return FileResponse("index.html")
